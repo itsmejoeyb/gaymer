584bcd5d660fec48e5e990de145034352e9dc9c5
itsmejoeyb <31454088+itsmejoeyb@users.noreply.github.com>
2020-05-26 16:11:13 -0400

    Deleted personal bot commands, just music commands

--- commands/cuddle.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'cuddle',
-    description: 'If you need a little snuggle.',
-    execute(message, args) {
-        message.channel.send(`${message.author} :kiss_mm: :eggplant: :doughnut:`)
-    },
-}
